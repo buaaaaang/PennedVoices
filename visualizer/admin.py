d4cc9e1df1c5f23464d29c7e3dda752e21403440
buaaaaang <buaaaaang@kaist.ac.kr>
2023-10-19 19:43:48 +0900

    won't use backend

--- visualizer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Visualizer
-from .models import Minter
-
-# Register your models here.
-
-admin.site.register(Visualizer)
-admin.site.register(Minter)
